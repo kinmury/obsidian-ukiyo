8c380d0a71b990750974cf7fc8da006ee6ca10b8
Kinmury <kinmury.ditmair@outlook.com>
2022-08-20 12:20:47 +0200

    Vault cleaning and some minor last minute fixes

--- testbench_vault/.trash/obsidian-hub-main/.github/scripts/tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-import utils
-
-
-def test_get_root_of_vault() -> None:
-    root = utils.get_root_of_vault()
-    contributing = os.path.join(root, 'CONTRIBUTING.md')
-    assert os.path.exists(contributing), contributing
